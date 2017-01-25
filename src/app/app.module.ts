11113af192f32b030deaf94c21041473cb6084ae
Davi Pereira <xdavipereira@gmail.com>
2017-01-25 16:56:59 -0200

    Adicionando lib de swipe, criando a tela de mensages e ultimas combinações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { MessagePage } from '../pages/message/message.component';
 import { HomePage } from '../pages/home/home';
 import { UserService } from '../pages/home/user.service';
 import { TabsPage } from '../pages/tabs/tabs';
 import { HttpModule }    from '@angular/http';
 import { SwingModule } from 'angular2-swing';
+import {KSSwiperModule} from 'angular2-swiper';
 import { UserModal} from '../pages/user/usermodal';
 
 
@@ -15,7 +16,7 @@ import { UserModal} from '../pages/user/usermodal';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    MessagePage,
     HomePage,
     TabsPage,
     UserModal
@@ -23,13 +24,14 @@ import { UserModal} from '../pages/user/usermodal';
   imports: [
     IonicModule.forRoot(MyApp,{ tabsPlacement: 'top' }),
     HttpModule,
-    SwingModule
+    SwingModule,
+    KSSwiperModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    MessagePage,
     HomePage,
     UserModal,
     TabsPage
